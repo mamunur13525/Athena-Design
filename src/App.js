bbb340a15f09da4123f366b382f692d6c6f70f0a
abuhorainhero <abuhorain2016@gmail.com>
2020-11-19 09:31:20 +0600

    some update

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Achievement from './Achievement/Achievement';
 import Home from './components/Home/Home';
 
 
 function App() {
   return (
     <>
-      <Achievement />
+      
      <Home></Home>
    
     </>
---
+++ src/components/Home/DedicatedTeam/DedicatedTeam.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const DedicatedTeam = () => {
+    return (
+        <section className='container'>
+            <h1 className='text-center'>Choose Your Dedicated Team</h1>
+
+        </section>
+    );
+};
+
+export default DedicatedTeam;
