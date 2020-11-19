07a1379f3cbf9380fdb067bd1baf0b601c0cc6a0
abuhorainhero <abuhorain2016@gmail.com>
2020-11-19 09:03:09 +0600

    achivement added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Achievement from './Achievement/Achievement';
 
 
 function App() {
   return (
     <>
-     
+      <Achievement />
    
     </>
   );
