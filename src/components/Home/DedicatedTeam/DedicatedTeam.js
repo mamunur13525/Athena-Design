7c6697025d0880cfa48c139e2e2732d784839171
abuhorainhero <abuhorain2016@gmail.com>
2020-11-19 10:23:08 +0600

    Choose team updated

--- src/components/Home/DedicatedTeam/DedicatedTeam.js
+++ src/components/Home/DedicatedTeam/DedicatedTeam.js
@@ -2,9 +2,52 @@ import React from 'react';
 
 const DedicatedTeam = () => {
     return (
-        <section className='container'>
+        <section className='container my-5'>
             <h1 className='text-center'>Choose Your Dedicated Team</h1>
-
+            <div className="row my-3">
+                <div className="col-md-4">
+                    <div className="">
+                        <h1>$199</h1>
+                        <p>For Basic</p>
+                        <br />
+                        <p>HomePage</p>
+                        <p>No Inner Page</p>
+                        <p>Asset Files</p>
+                        <p>Source Files</p>
+                        <p>Free Stock Photos</p>
+                        <p>10 Days Free Support</p>
+                        <p>24/7 Support</p>
+                    </div>
+                </div>
+                <div className="col-md-4">
+                    <div className="">
+                        <h1>$399</h1>
+                        <p>For Preferred</p>
+                        <br />
+                        <p>HomePage</p>
+                        <p>4 Inner Page</p>
+                        <p>Asset Files</p>
+                        <p>Source Files</p>
+                        <p>Free Stock Photos</p>
+                        <p>20 Days Free Support</p>
+                        <p>24/7 Support</p>
+                    </div>
+                </div>
+                <div className="col-md-4">
+                    <div className="">
+                        <h1>$599</h1>
+                        <p>For Elite</p>
+                        <br />
+                        <p>HomePage</p>
+                        <p>8 Inner Page</p>
+                        <p>Asset Files</p>
+                        <p>Source Files</p>
+                        <p>Free Stock Photos</p>
+                        <p>30 Days Free Support</p>
+                        <p>24/7 Support</p>
+                    </div>
+                </div>
+            </div>
         </section>
     );
 };
